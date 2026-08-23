c6351dd3f85acca81b24e35c970651c74f1e1564
Aman <suleymenov.aqtobe@gmail.com>
2026-08-23 16:11:17 +0200

    Moved files to match the task spec

--- mazegen/main.py
+++ mazegen/main.py
@@ -1,5 +1,20 @@
+import sys
+
+
 def main():
     print("Hello from mazegen!")
+    if len(sys.argv) != 2:
+        print(
+            "Provide only 1 argument, which is a config file name:\n"
+            "E.g.: `python3 a_maze_ing.py config.txt`",
+            file=sys.stderr,
+        )
+        sys.exit(1)
+    try:
+        config_dict = parse_config(sys.argv[1].strip())
+        print(config_dict)
+    except (FileNotFoundError, ConfigError) as e:
+        print(f"Error parsing configs with the provided filename:\n{e}")
 
 
 if __name__ == "__main__":
