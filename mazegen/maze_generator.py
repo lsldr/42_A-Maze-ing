d46435c90b3aa2c2c2a40de999d6db687d4fcf29
Aman <suleymenov.aqtobe@gmail.com>
2026-08-23 16:07:03 +0200

    Saving some WIP for the structure for mazegen and 42 pattern mark-up

--- mazegen/maze_generator.py
+++ mazegen/maze_generator.py
@@ -1,11 +1,26 @@
 from typing import Dict, Any
+from random import randint
 
 
 def grid_builder(configs: Dict[str, Any]) -> list[list[int]]:
     grid = list[list[int]]
     print(configs)
+    w = configs.get("width")
+    h = configs.get("height")
 
-    grid = list([[15] * configs.get("width")] * configs.get("height"))
+    grid = list([[15] * w] * h)
+
+    if w >= 9 or h >= 7:
+        immutable = block_ft_pattern(grid)
+    else:
+        print(
+            "Warning: maze dimensions are too small for the 42 pattern!\n"
+            "Generating a maze without the 42 pattern."
+        )
 
     print(grid)
     return grid
+
+
+def block_ft_pattern(grid: list[list[int]]) -> set[tuple[int, int]]:
+    immutable = set()
