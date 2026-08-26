bf5f9d18486f1cb201301fb918bffbf6d90e652a
Aman <suleymenov.aqtobe@gmail.com>
2026-08-26 11:06:29 +0200

    Fixed the algorithm functionality location for BFS. Need to plug it on to the core Maze object

--- mazegen/src/mazegen/pathfinder/bfs.py
+++ mazegen/src/mazegen/pathfinder/bfs.py
@@ -1,20 +1,60 @@
 from mazegen.maze import Maze
 from mazegen.pathfinder.pathfinder import Pathfinder
 from mazegen.util import Point
+from mazegen.cell import Wall
+
+from collections import deque
 
 
 class BFS_pathfinder(Pathfinder):
     def __init__(self, maze: Maze) -> None:
         super().__init__(maze)
-        self._stack: list[Point] = []
+        self._queue: deque[Point] = deque([self.maze.entry])
+        self._visited: set[Point] = {self.maze.entry}
+        self._parent: dict[Point, Point] = {}
+        self._explored: list[Point] = [self.maze.entry]
+
+    def _reconstruct_path(self) -> list[Point]:
+        """Traces from exit point to entry using the parent dictionary
+
+        Returns the list of points for the entry-to-exit path"""
+
+        path: list[Point] = []
+        c_point: Point | None = self.maze.exit
+        while c_point is not None and c_point != self.maze.entry:
+            path.append(c_point)
+            c_point = self._parent.get(c_point)
+
+        path.append(self.maze.entry)
+        path.reverse()
+        return path
 
     def next(self) -> tuple[bool, list[Point]]:
-        if self._done:
-            return True, self._stack.copy()
-        if not self._stack:
-            self._stack.append(self.maze.entry)
-            if self.maze.entry == self.maze.exit:
-                self._done = True
-                self._path.append(self.maze.entry)
-            return self._done, self._stack.copy()
-        return False, self._stack
+        all_sides = [Wall.NORTH, Wall.EAST, Wall.SOUTH, Wall.WEST]
+        if self._done or not self._queue:
+            self._done = True
+            return True, self._explored
+
+        current = self._queue.popleft()
+
+        # If exit reached
+        if current == self.maze.exit:
+            self._path = self._reconstruct_path()
+            self._done = True
+            return True, self._explored
+
+        c_cell = self.maze.get_cell(current)
+        for side in all_sides:
+            if side not in c_cell.wall:
+                dx, dy = side.get_direction()
+                npos = Point(current.x + dx, current.y + dy)
+
+                if self.maze.in_bounds(npos) and npos not in self._visited:
+                    self._visited.add(npos)
+                    self._parent[npos] = current
+                    self._queue.append(npos)
+                    self._explored.append(npos)
+        if not self._queue:
+            self._done = True
+            return True, self._explored
+        return False, self._explored
--- mazegen/src/mazegen/pathfinder/pathfinder.py
+++ mazegen/src/mazegen/pathfinder/pathfinder.py
@@ -1,84 +1,34 @@
 from abc import ABC, abstractmethod
 from mazegen.maze import Maze
 from mazegen.util import Point
-from collections import deque
 
 
 class Pathfinder(ABC):
     def __init__(self, maze: Maze) -> None:
         self.maze = maze
         self._done = False
-        self._queue: list[Point] = deque([self.maze.entry])
-        self._visited: set[Point] = {self.maze.entry}
-        self._parent: dict[Point, Point] = dict()
-        self._explored: list[Point] = [self.maze.entry]  # For UI visuals
         self._path: list[Point] = []
 
     def is_done(self) -> bool:
-        """Check if path was found
-
-        Returns:
-            True if path was found, False otherwise
-        """
+        """Check if path was found."""
         return self._done
 
     def get_path(self) -> list[Point]:
-        """Returns shortest path from start to end
-        or empty if path is not ready"""
+        """Returns shortest path from start to end, or empty if not ready."""
         return self._path
-
-    @property
-    def queue(self) -> list[Point]:
-        """Returns the queue of cells to explore next"""
-        return self._queue
-
-    @property
-    def explored(self) -> list[Point]:
-        """Returns the list of explored cells"""
-        return self._explored
-
-    @property
-    def visited(self) -> set[Point]:
-        """Returns the set of visited cells"""
-        return self._visited
-
-    @property
-    def parent(self) -> dict[Point, Point]:
-        """Returns the parent dictionary of cells"""
-        return self._parent
 
     @abstractmethod
     def next(self) -> tuple[bool, list[Point]]:
-        """Generate next step in pathfinding
+        """Generate next step in pathfinding.
 
         Returns:
-            bool if a path was found
-            list of checked spaces
+            bool: True if pathfinding finished, False otherwise.
+            list[Point]: List of explored/checked spaces for visualization.
         """
+        pass
 
-        if self.is_done() or not self._queue:
-            return (True, self._explored)
-
-        current = self._queue.popleft()
-        self._explored.append(current)
-
-        if current == self.maze.exit:
-            while current != self.maze.entry:
-                current = self._parent["current"]
-            self._path = self._explored.reverse()
-            self._done = True
-            return (True, self._explored)
-
-        return (False, self._explored)
-
-    @abstractmethod
     def finish(self) -> list[Point]:
-        """Finish maze generation and return finished path
-
-        Returns:
-            shortest path
-        """
-        done, _ = self.next()
-        while not done:
-            done, _ = self.next()
+        """Run pathfinding to completion and return the path."""
+        while not self.is_done():
+            self.next()
         return self._path
